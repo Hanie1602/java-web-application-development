4916caca7f190b4d1bb038ffd07ce7392e85f658
TDHanie <luongngocthuyduongvt@gmail.com>
2024-05-27 22:19:10 +0700

    Order DTO

--- MVC2/src/java/duonglnt/tOrder/TOrderDTO.java
+++ MVC2/src/java/duonglnt/tOrder/TOrderDTO.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package duonglnt.tOrder;
 
 import java.io.Serializable;
 import java.sql.Date;
 
-/**
- *
- * @author trung
- */
 public class TOrderDTO implements  Serializable{
     private String id;
     private Date date;
