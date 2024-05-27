0cd7f099841801abaf2d77b89c838a480341e20f
TDHanie <luongngocthuyduongvt@gmail.com>
2024-05-27 22:09:09 +0700

    Order Detail DAO

--- MVC2/src/java/duonglnt/orderDetail/OrderDetailDAO.java
+++ MVC2/src/java/duonglnt/orderDetail/OrderDetailDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package duonglnt.orderDetail;
 
 import duonglnt.util.DBHelper;
@@ -11,10 +6,6 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import javax.naming.NamingException;
 
-/**
- *
- * @author trung
- */
 public class OrderDetailDAO {
     public boolean insertOrderDetail(OrderDetailDTO detailDTO)
             throws SQLException, NamingException {
