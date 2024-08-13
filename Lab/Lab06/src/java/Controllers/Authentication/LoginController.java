7281791564b367ef3580c234b96bc7576218769f
TDHanie <luongngocthuyduongvt@gmail.com>
2024-08-13 19:53:18 +0700

    Manage User By MVC2

---
+++ Lab/Lab06/src/java/Controllers/Authentication/LoginController.java
@@ -0,0 +1,88 @@
+package Controllers.Authentication;
+
+import Models.DAO.UserDAO;
+import Models.DTO.User;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name = "LoginController", urlPatterns = {"/LoginController"})
+public class LoginController extends HttpServlet {
+
+    private final String searchPage = "Search.jsp";
+    private final String userController = "UserController";
+    private final String loginPage = "Login.jsp";
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String url = loginPage;
+        String userName = request.getParameter("txtUserName");
+        String password = request.getParameter("txtPassword");
+        try {
+            UserDAO userDao = new UserDAO();
+            User user = userDao.login(userName, password);
+            if (user != null) {
+                HttpSession session = request.getSession();
+                session.setAttribute("UserLoggedIn", user);
+                if (user.isIsAdmin()) {
+                    url = searchPage;
+                } else {
+                    url = userController + "?action=Details&&UserName=" + userName;
+                }
+            } else {
+                request.setAttribute("message", "The username or password is invalid.");
+            }
+        } catch (Exception ex) {
+            log(ex.getMessage());
+        } finally {
+            RequestDispatcher rd = request.getRequestDispatcher(url);
+            rd.forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
