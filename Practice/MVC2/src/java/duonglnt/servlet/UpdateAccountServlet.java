155e8e9e7002a43fab6612455ec097dc5fb815e2
TDHanie <luongngocthuyduongvt@gmail.com>
2024-08-14 12:19:03 +0700

    Update Account Servlet

---
+++ Practice/MVC2/src/java/duonglnt/servlet/UpdateAccountServlet.java
@@ -0,0 +1,99 @@
+
+package duonglnt.servlet;
+
+import duonglnt.registration.RegistrationDAO;
+import java.io.IOException;
+import java.sql.SQLException;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "UpdateAccountServlet", urlPatterns = {"/UpdateAccountServlet"})
+public class UpdateAccountServlet extends HttpServlet {
+    private final String ERRORS_PAGE = "errors.html";
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        //1. get all parameters
+        String username = request.getParameter("txtUsername");
+        String password = request.getParameter("txtPassword");
+        boolean isRole = request.getParameter("chkAdmin") != null;
+        String searchVale = request.getParameter("lastSearchValue");
+        String url = ERRORS_PAGE;
+        
+        try  {
+            //2. call Model/DAO
+            //2.1 new DAO Object
+            RegistrationDAO dao = new RegistrationDAO();
+            //2.2 Call method of DAO
+            boolean result = dao.updateAccount(username, password, isRole);  
+            //3. Process result
+            if (result){
+                url = "DispatchServlet"
+                        + "?btAction=Search"
+                        + "&txtSearchValue=" + searchVale;
+            } // end update
+        } catch(SQLException ex){
+            ex.printStackTrace();
+        } catch (NamingException ex){
+            ex.printStackTrace();
+        } finally {
+            response.sendRedirect(url);
+        }
+    }
+
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
