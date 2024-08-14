4a46726c02d68256f56b17f6b8cda0a9440ac40f
TDHanie <luongngocthuyduongvt@gmail.com>
2024-08-14 12:18:35 +0700

    Search Lastname Servlet

---
+++ Practice/MVC2/src/java/duonglnt/servlet/SearchLastnameServlet.java
@@ -0,0 +1,96 @@
+package duonglnt.servlet;
+
+import duonglnt.registration.RegistrationDAO;
+import duonglnt.registration.RegistrationDTO;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import javax.naming.NamingException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "SearchLastnameServlet", urlPatterns = {"/SearchLastnameServlet"})
+public class SearchLastnameServlet extends HttpServlet {
+
+    private final String SEARCH_PAGE = "search.html";
+    private final String RESULT_PAGE = "search.jsp";
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        //1. get all Client information
+        String searchValue = request.getParameter("txtSearchValue"); 
+        String url = SEARCH_PAGE;
+
+        try {
+            System.out.println("Controller");
+            if (!searchValue.trim().isEmpty()) {
+                System.out.println("Bat dau Search");
+                //2. call Model/DAO
+                //2.1 New DAO obj
+                RegistrationDAO dao = new RegistrationDAO();
+                //2.2 Call method of DAO
+                dao.searchLastname(searchValue);
+                List<RegistrationDTO> result = dao.getAccounts();
+                //3. Process result
+                url = RESULT_PAGE;
+                request.setAttribute("SEARCH_RESULT", result);
+            } //end searchValue has inputted valid value
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (NamingException ex) {
+            ex.printStackTrace();
+        } finally {
+            //4. Send to View (+ request scope)
+            RequestDispatcher rd = request.getRequestDispatcher(url);
+            rd.forward(request, response);
+        }
+    }
+
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
